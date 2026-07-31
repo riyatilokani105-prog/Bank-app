f1a01cbff0908bc1211d6d70af8afb6da01aa673
riyatilokani105-prog <riyatilokani105@gmail.com>
2026-07-31 21:27:23 +0530

    updated customer statement api again

--- client/src/api/customerStatementApi.js
+++ client/src/api/customerStatementApi.js
@@ -1,9 +1,20 @@
-import API from "./axios";
+import axios from "./axios";
 
 export const getCustomerStatement = async (search) => {
-  const { data } = await axiosInstance.get(
-    `/customer-statement?search=${search}`
+  const { data } = await axios.get(
+    `/customer-statement?search=${encodeURIComponent(search)}`
   );
 
   return data;
+};
+
+export const downloadCustomerStatement = async (search) => {
+  const response = await axios.get(
+    `/customer-statement/pdf?search=${encodeURIComponent(search)}`,
+    {
+      responseType: "blob",
+    }
+  );
+
+  return response.data;
 };
