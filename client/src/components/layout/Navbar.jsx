44c5e6a16a6cc3d059c233d0ad1c876487b789e6
riyatilokani105-prog <riyatilokani105@gmail.com>
2026-08-05 21:58:46 +0530

    removed admin page

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -7,9 +7,6 @@ import {
 } from "react-icons/fa";
 
 const Navbar = ({ toggleSidebar }) => {
-  const profileImage = "/images/admin-profile.jpg"; // Replace with the actual path to your profile image
-
-
   return (
     <header className="navbar">
       <div className="navbar-left">
@@ -39,17 +36,16 @@ const Navbar = ({ toggleSidebar }) => {
 
         {/* Profile */}
 
-        <div
-  className="admin-profile"
-  onClick={() => navigate("/admin-profile")}
->
-    <img src={profileImage} alt="Admin" />
+        <div className="profile">
 
-    <div>
-        <h4>Administrator</h4>
-        <p>Admin</p>
-    </div>
-</div>
+          <FaUserCircle className="profile-icon" />
+
+          <div className="profile-info">
+            <h4>Administrator</h4>
+            <p>Admin</p>
+          </div>
+
+        </div>
 
       </div>
     </header>
